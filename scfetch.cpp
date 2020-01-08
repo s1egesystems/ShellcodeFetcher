3ae5e8e0d9ac844e431f551603696afa68e088a9
s1egesystems <51238001+s1egesystems@users.noreply.github.com>
2020-01-08 09:28:39 +0000

    Update scfetch.cpp

--- scfetch.cpp
+++ scfetch.cpp
@@ -1,7 +1,7 @@
 #include <winsock2.h>
 #include "windows.h"
 #include <iostream> 
-
+// Coded by s1ege greetz to all GSH members
 #pragma comment(lib,"ws2_32.lib")
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
