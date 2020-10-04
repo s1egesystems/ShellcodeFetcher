9faaa905911e403d5cd4f2a4e2ae7121e5f95034
s1egesystems <51238001+s1egesystems@users.noreply.github.com>
2020-10-04 05:54:37 +0000

    Update scfetch.cpp

--- scfetch.cpp
+++ scfetch.cpp
@@ -1,7 +1,8 @@
 // Coded by s1ege greetz to all #GSH members.
 #include <winsock2.h>
 #include <windows.h>
-#include <iostream> 
+#include <string>
+//#include <iostream>
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "user32.lib")
